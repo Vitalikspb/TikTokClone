cb8d51426e0322381cee3bd6835a3c6f87c32c54
Vitalikspb <hotmail-88@mail.ru>
2021-07-22 19:52:09 +0300

    Create ScrollView on homeVC

---
+++ TikTok/TikTok/Models/PostModel.swift
@@ -0,0 +1,22 @@
+//
+//  PostModel.swift
+//  TikTok
+//
+//  Created by VITALIY SVIRIDOV on 22.07.2021.
+//
+
+import Foundation
+
+struct PostModel {
+    let identifier: String
+    
+    static func mockModels() -> [PostModel] {
+        var posts = [PostModel]()
+        for _ in 0...100 {
+            let post = PostModel(identifier: UUID().uuidString)
+            posts.append(post)
+        }
+        return posts
+    }
+    
+}
