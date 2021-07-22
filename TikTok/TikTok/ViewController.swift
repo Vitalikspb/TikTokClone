5727060ef586f1809ca0b9266c9fa9839bde9958
Vitalikspb <hotmail-88@mail.ru>
2021-07-22 13:21:29 +0300

    initial

---
+++ TikTok/TikTok/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TikTok
+//
+//  Created by VITALIY SVIRIDOV on 22.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
